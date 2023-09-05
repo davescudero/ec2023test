fef71eba399ad849f32397c9d0165cdc39e4dc36
David Escudero García <davidescudero@MacBook-Pro-de-David.local>
2023-09-04 19:17:30 -0600

    Agregando mi_script_numpy.py que incluye numpy

---
+++ mi_script_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def crear_array():
+    # Crear un array simple con numpy
+    arr = np.array([1, 2, 3, 4, 5])
+    return arr
+
+if __name__ == "__main__":
+    array_resultado = crear_array()
+    print("Array creado con Numpy:", array_resultado)
+
